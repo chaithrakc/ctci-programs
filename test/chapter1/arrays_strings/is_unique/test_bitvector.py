000f0121347336bc5748f1982c296f931a339362
Chaithra K C <chaithra.kc22@gmail.com>
2021-06-17 16:53:22 +0530

    test cases

--- test/chapter1/arrays_strings/is_unique/test_bitvector.py
+++ test/chapter1/arrays_strings/is_unique/test_bitvector.py
@@ -1,16 +1,16 @@
 import pytest
 
-from ctcl.chapter1.arrays_strings.is_unique.bruteforce_approach import Solution
+from ctcl.chapter1.arrays_strings.is_unique.bitvector import Solution
 from ctcl.commons.step_input import StepInput
 from test.chapter1.arrays_strings.is_unique.data import get_test_data
 
 
 @pytest.mark.parametrize("word,is_unique", get_test_data())
-def test_bruteforce(word, is_unique):
+def test_space_optimization(word,is_unique):
     step_input = StepInput()
     step_input.set_input('word', word)
     solution = Solution()
     solution.set_step_input(step_input)
-    solution.bruteforce()
+    solution.space_optimization()
     step_output = solution.get_step_output()
     assert step_output.get_output('word') is is_unique, step_input.get_input('word') + ' does not have unique letters'
