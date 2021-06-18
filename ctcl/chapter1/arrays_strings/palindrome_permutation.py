0da84a5da2f6aed12bbe0b8c10ac5c88e89a2cf5
Chaithra K C <chaithra.kc22@gmail.com>
2021-06-18 19:23:12 +0530

    removing use of additional data structures

--- ctcl/chapter1/arrays_strings/palindrome_permutation.py
+++ ctcl/chapter1/arrays_strings/palindrome_permutation.py
@@ -1,23 +1,33 @@
-def isvalid_palindrome_permutation_dicts(word: str) -> bool:
-    word = str.lower(word)  # to make case insensitive
-    word = word.replace(' ', '')  # removing the spaces
-    letter_count = dict()
-    for letter in word:
-        letter_count[letter] = letter_count.get(letter, 0) + 1
+def isvalid_palindrome_permutation_dicts(phrase: str) -> bool:
+    phrase = str.lower(phrase)  # to make case insensitive
+    phrase = phrase.replace(' ', '')  # removing the spaces
+    letter_count = build_charfreq_table(phrase)
+    return check_maxone_odd(letter_count)
+
+
+def build_charfreq_table(phrase: str) -> list:
+    freq_table = [0] * (ord('z') - ord('a') + 1)
+    for char in phrase:
+        index = ord(char) - ord('a')
+        freq_table[index] = freq_table[index] + 1
+    return freq_table
+
+
+def check_maxone_odd(table: list) -> bool:
     odd_counter = False  # check whether all letters have even count except for at most one letter.
-    for letter in word:
-        if letter_count.get(letter) % 2:
+    for count in table:
+        if count % 2:
             if odd_counter:
                 return False
             odd_counter = True
     return True
 
 
-def isvalid_palindrome_permutation_bitvector(word: str) -> bool:
-    word = str.lower(word)  # to make case insensitive
-    word = word.replace(' ', '')  # removing the spaces
+def isvalid_palindrome_permutation_bitvector(phrase: str) -> bool:
+    phrase = str.lower(phrase)  # to make case insensitive
+    phrase = phrase.replace(' ', '')  # removing the spaces
     checker = 0
-    for letter in word:
+    for letter in phrase:
         val = ord(letter) - ord('a')
         checker = checker ^ (1 << val)
-    return bin(checker).count('1') == len(word) % 2  # all letters have even count zero except for at most one letter
+    return bin(checker).count('1') == len(phrase) % 2  # all letters have even count zero except for at most one letter
