d46844bb3c12248c1d744909bbe2149544e6e9fd
Chaithra K C <chaithra.kc22@gmail.com>
2021-06-18 16:30:43 +0530

    refactoring name to anagram

---
+++ ctcl/chapter1/arrays_strings/palindrome_permutation.py
@@ -0,0 +1,13 @@
+def isvalid_palindrome_permutation_dicts(word: str) -> bool:
+    word = str.lower(word)  # to make case insensitive
+    word = word.replace(' ', '')  # removing the spaces
+    letter_count = dict()
+    for letter in word:
+        letter_count[letter] = letter_count.get(letter, 0) + 1
+    odd_counter = False  # treat it like a flag variable
+    for letter in word:
+        if letter_count.get(letter) % 2:
+            if odd_counter:
+                return False
+            odd_counter = True
+    return True
---
+++ test/chapter1/arrays_strings/test_palindrome_permutation.py
@@ -0,0 +1,21 @@
+import pytest
+
+from ctcl.chapter1.arrays_strings.palindrome_permutation import isvalid_palindrome_permutation_dicts
+
+
+def get_testdata():
+    test_data = [
+        ('Tact Coa', True),  # taco cat
+        ('Wikipedia', False),
+        ('diervriDe', True),  # Redivider
+        ('iervrvse', False),   # revivers
+        ('reviverst', False),  # revivers
+        ('dog am I dogma', True),  # dogma I am god
+        ('ielv emit no on emit evil', True)  #Live on time, emit no evil
+    ]
+    return test_data
+
+
+@pytest.mark.parametrize("test_data, expected", get_testdata())
+def test_isvalid_palindrome_permutation(test_data, expected):
+    assert isvalid_palindrome_permutation_dicts(test_data) == expected
