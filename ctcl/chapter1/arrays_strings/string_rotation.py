6b6d39664d6f8c9bab3182d33a14282ca5522429
Chaithra K C <chaithra.kc22@gmail.com>
2021-06-28 00:27:09 +0530

    string rotation problem solution

--- ctcl/chapter1/arrays_strings/string_rotation.py
+++ ctcl/chapter1/arrays_strings/string_rotation.py
@@ -1,13 +1,27 @@
+'''
+String Rotation:Assumeyou have a method isSubstringwhich checks if oneword is a substring 
+of another. Given two strings, sl and s2, write code to check if s2 is a rotation of sl using only one 
+call to isSubstring (e.g., "waterbottle" is a rotation of"erbottlewat"). 
+Hints: #34, #88, # 7 04
+
+Difficulty : Easy
+'''
 class SolutionStringRotation:
-    __str1 = ''
-    __str2 = ''
+    __first_string = ''
+    __second_string = ''
 
-    def set_input(self, s1: str, s2: str) -> None:
-        self.__str1 = s1
-        self.__str2 = s2
+    def set_input(self, first_string: str, second_string: str) -> None:
+        self.__first_string = first_string
+        self.__second_string = second_string
 
-    def __is_substring(self) -> bool:
-        pass  # to do
+    def __is_substring(self, string: str, sub_string: str) -> bool:
+        return True if string.find(sub_string) > -1 else False
 
-    def valid_rotation(self) -> bool:
-        pass  # to do
+    def are_rotated_bruteforce(self) -> bool:
+        # both the strings are same -> they are not rotated
+        # both the strings have different lengths -> they are not rotated
+        if self.__first_string == self.__second_string or len(self.__first_string) != len(self.__second_string):
+            return False
+        repeated_first_str = self.__first_string*2
+        return self.__is_substring(repeated_first_str, self.__second_string)
+                   
--- test/chapter1/arrays_strings/test_string_rotation.py
+++ test/chapter1/arrays_strings/test_string_rotation.py
@@ -5,7 +5,12 @@ from ctcl.chapter1.arrays_strings.string_rotation import SolutionStringRotation
 
 def get_testcases():
     return [
-        ('waterbottle', 'erbottlewat', True)
+        ('waterbottle', 'erbottlewat', True),
+        ('recursion','sionrecur',True),
+        ('1234','4321', False),
+        ('1234','2341', True),
+        ('hersheys','heyshers', True),
+        ('erererer','rererere', True)
     ]
 
 
@@ -15,4 +20,4 @@ class TestSolutionStringRotation:
     @pytest.mark.parametrize('s1,s2,expected', get_testcases())
     def test_valid_rotation(self, s1: str, s2: str, expected: bool):
         self.__solution.set_input(s1, s2)
-        assert self.__solution.valid_rotation() == expected
+        assert self.__solution.are_rotated_bruteforce() == expected
