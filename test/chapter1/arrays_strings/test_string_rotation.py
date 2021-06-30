c8576deb1ea67cb53897881290fce4ca0031148c
Chaithra K C <chaithra.kc22@gmail.com>
2021-07-01 03:32:19 +0530

    test cases modification

--- test/chapter1/arrays_strings/test_string_rotation.py
+++ test/chapter1/arrays_strings/test_string_rotation.py
@@ -6,11 +6,12 @@ from ctci.chapter1.arrays_strings.string_rotation import SolutionStringRotation
 def get_testcases():
     return [
         ('waterbottle', 'erbottlewat', True),
-        ('recursion','sionrecur',True),
-        ('1234','4321', False),
-        ('1234','2341', True),
-        ('hersheys','heyshers', True),
-        ('erererer','rererere', True)
+        ('recursion', 'sionrecur', True),
+        ('1234', '4321', False),
+        ('1234', '2341', True),
+        ('hersheys', 'heyshers', True),
+        ('erererer', 'rererere', True),
+        ('mobile', 'ilemobil', False)
     ]
 
 
@@ -18,6 +19,6 @@ class TestSolutionStringRotation:
     __solution = SolutionStringRotation()
 
     @pytest.mark.parametrize('s1,s2,expected', get_testcases())
-    def test_valid_rotation(self, s1: str, s2: str, expected: bool):
+    def test_are_rotated(self, s1: str, s2: str, expected: bool):
         self.__solution.set_input(s1, s2)
         assert self.__solution.are_rotated() == expected
