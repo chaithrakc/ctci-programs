a2994ab241dcfc7c4bedd8759f6552c62ea6da16
Chaithra K C <chaithra.kc22@gmail.com>
2021-07-17 03:12:11 +0530

    intersection : test cases impl

--- test/chapter2/linked_lists/test_p07_intersection.py
+++ test/chapter2/linked_lists/test_p07_intersection.py
@@ -5,34 +5,32 @@ from commons.linked_list import LinkedList, Node
 
 
 def get_tests():
-    testcase1 = get_intersectionlists(LinkedList('a1', 'a2', 'c1', 'c2', 'c3'), LinkedList('b1', 'b2', 'b3'), 3)
-    testcase2 = get_intersectionlists(LinkedList(4, 1, 8, 4, 5), LinkedList(5, 6, 1), 3)
-    testcase3 = get_intersectionlists(LinkedList(1, 9, 1, 2, 4), LinkedList(3), 4)
-    testcase4 = get_intersectionlists(LinkedList(2, 6, 4), LinkedList(1, 5), 0)
+    testcase1 = get_intersect_lists(LinkedList('a1', 'a2'), LinkedList('b1', 'b2', 'b3'), LinkedList('c1', 'c2', 'c3'))
+    testcase2 = get_intersect_lists(LinkedList(4, 1), LinkedList(5, 6, 1), LinkedList(8, 4, 5))
+    testcase3 = get_intersect_lists(LinkedList(1, 9, 1), LinkedList(3), LinkedList(2, 4))
+    testcase4 = get_intersect_lists(LinkedList(2, 6, 4), LinkedList(1, 5), LinkedList())
     return [(testcase1[0], testcase1[1], testcase1[2]),
             (testcase2[0], testcase2[1], testcase2[2]),
             (testcase3[0], testcase3[1], testcase3[2]),
             (testcase4[0], testcase4[1], testcase4[2])]
 
 
-def get_intersectionlists(list1: LinkedList, list2: LinkedList, node_count: int) -> list:
-    if not node_count:
-        return [list1, list2, 0]
+def get_intersect_lists(list1: LinkedList, list2: LinkedList, intersect_list: LinkedList) -> list:
     node1 = list1.head
     node2 = list2.head
-    for _ in range(node_count - 1):
+    while node1.next:
         node1 = node1.next
-    intersection_node = node1
     while node2.next:
         node2 = node2.next
-    node2.next = node1
-    return [list1, list2, intersection_node]
+    node1.next = intersect_list.head
+    node2.next = intersect_list.head
+    return [list1.head, list2.head, intersect_list.head]
 
 
 class TestSolutionIntersection:
     solution = SolutionIntersection()
 
-    @pytest.mark.parametrize('linkedlist1, linkedlist2, intersecting_node', get_tests())
-    def test_get_intersection_node(self, linkedlist1, linkedlist2, intersecting_node):
-        node = self.solution.get_intersection_node(linkedlist1.head, linkedlist2.head)
-        assert intersecting_node == node
+    @pytest.mark.parametrize('head1, head2, intersect_node', get_tests())
+    def test_get_intersection_node(self, head1: Node, head2: Node, intersect_node: Node) -> None:
+        node = self.solution.get_intersection_node(head1, head2)
+        assert intersect_node == node
