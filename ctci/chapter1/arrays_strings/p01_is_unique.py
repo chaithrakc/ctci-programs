5f414e8598c6363ffea3389b919d7ed1702ccdec
Chaithra K C <chaithra.kc22@gmail.com>
2023-03-31 13:14:43 -0400

    updating the code

--- ctci/chapter1/arrays_strings/p01_is_unique.py
+++ ctci/chapter1/arrays_strings/p01_is_unique.py
@@ -9,29 +9,35 @@ Difficulty: Easy
 
 
 class SolutionUnique:
-    __word = ''
+    word = ''
 
     def set_input(self, word: str) -> None:
-        self.__word = word
+        self.word = word
 
     # assuming ascii set has only 128 characters
     # space complexity is high, O(n) time complexity
     def ascii_approach(self, ascii_set=128) -> bool:
-        if len(self.__word) > ascii_set:
-            return False
-        ascii_histogram = [False] * ascii_set
-        for char in self.__word:
-            ascii_value = ord(char)
-            if ascii_histogram[ascii_value]:
+        # if len(self.word) > ascii_set:
+        #     return False
+        # ascii_histogram = [False] * ascii_set
+        # for char in self.word:
+        #     ascii_value = ord(char)
+        #     if ascii_histogram[ascii_value]:
+        #         return False
+        #     ascii_histogram[ascii_value] = True
+        # return True
+        hashset = set()
+        for char in self.word:
+            if char in hashset:
                 return False
-            ascii_histogram[ascii_value] = True
+            hashset.add(char)
         return True
 
     # Best Case : O(n) but limited only to lower case letters.
     # space complexity is improved.
     def bitvector(self) -> bool:
         checker = 0
-        for char in self.__word:
+        for char in self.word:
             bit_index = ord(char) - ord('a')
             if checker & (1 << bit_index) > 0:
                 return False
@@ -40,8 +46,8 @@ class SolutionUnique:
 
     # worst case : O(n2)
     def bruteforce(self) -> bool:
-        for key_index in range(len(self.__word)):
-            for other_index in range(key_index + 1, len(self.__word)):
-                if self.__word[key_index] == self.__word[other_index]:
+        for key_index in range(len(self.word)):
+            for other_index in range(key_index + 1, len(self.word)):
+                if self.word[key_index] == self.word[other_index]:
                     return False
         return True
