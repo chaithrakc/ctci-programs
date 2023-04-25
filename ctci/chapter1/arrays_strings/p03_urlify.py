c0c1b359e4845d56d13039f6bd411e1fb85af8f0
Chaithra K C <chaithra.kc22@gmail.com>
2023-04-25 17:21:20 -0400

    Update p03_urlify.py

--- ctci/chapter1/arrays_strings/p03_urlify.py
+++ ctci/chapter1/arrays_strings/p03_urlify.py
@@ -9,17 +9,17 @@ Output: "Mr%20John%20Smith"
 Hints: #53, # 118
 '''
 class SolutionURLify:
-    __name = ''
-    __true_length = 0
+    name = ''
+    true_length = 0
 
     def set_input(self, name: str, true_length: int) -> None:
-        self.__name = name
-        self.__true_length = true_length
+        self.name = name
+        self.true_length = true_length
 
     def urlify(self) -> str:
-        char_arr = list(self.__name)
-        writer_index = len(self.__name)
-        for reader_index in range(self.__true_length - 1, -1, -1):
+        char_arr = list(self.name)
+        writer_index = len(self.name)
+        for reader_index in range(self.true_length - 1, -1, -1):
             if char_arr[reader_index].isspace():
                 char_arr[writer_index - 1] = '0'
                 char_arr[writer_index - 2] = '2'
